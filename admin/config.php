a5529d9232f47c6d299f6f8b6a54ab0ccf88df03
Greg Adams <greg@jitterbug.co.za>
2014-12-26 23:18:07 +0200

    Initial

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oddworx/admin/');
+define('HTTP_CATALOG', 'http://localhost/oddworx/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oddworx/admin/');
+define('HTTPS_CATALOG', 'http://localhost/oddworx/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\oddworx/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\oddworx/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\oddworx/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\oddworx/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\oddworx/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\oddworx/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\oddworx/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\oddworx/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\oddworx/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\oddworx/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\oddworx/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\oddworx/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oddworker');
+define('DB_PASSWORD', 'r0adk1ll');
+define('DB_DATABASE', 'oddworx_shop');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oddworx/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oddworx/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\oddworx/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\oddworx/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\oddworx/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\oddworx/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\oddworx/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\oddworx/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\oddworx/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\oddworx/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\oddworx/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\oddworx/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\oddworx/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oddworker');
+define('DB_PASSWORD', 'r0adk1ll');
+define('DB_DATABASE', 'oddworx_shop');
+define('DB_PREFIX', 'oc_');
